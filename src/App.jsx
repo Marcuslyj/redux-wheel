62c32e01547a0cf28bb4eafe0cc979022633ee83
liyongjie <liyongjie@youzan.com>
2022-03-21 18:13:48 +0800

    feat：精准render

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 // 请从课程简介里下载本代码
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 const appContext = React.createContext(null);
-export const App = () => {
-  const [appState, setAppState] = useState({
+const store = {
+  state: {
     user: { name: "frank", age: 18 },
-  });
-  const contextValue = { appState, setAppState };
+  },
+  setState(newState) {
+    store.state = newState;
+    store.listeners.map((fn) => fn(store.state));
+  },
+  listeners: [],
+  subscribe(fn) {
+    store.listeners.push(fn);
+    return () => {
+      const index = store.listeners.indexOf(fn);
+      store.listeners.splice(index, 1);
+    };
+  },
+};
+
+export const App = () => {
   return (
-    <appContext.Provider value={contextValue}>
+    <appContext.Provider value={store}>
       <大儿子 />
       <二儿子 />
       <幺儿子 />
@@ -16,22 +30,27 @@ export const App = () => {
   );
 };
 
-const 大儿子 = () => (
-  <section>
-    大儿子
-    <User />
-  </section>
-);
-const 二儿子 = () => (
-  <section>
-    二儿子
-    <UserModifier>内容</UserModifier>
-  </section>
-);
-const 幺儿子 = () => <section>幺儿子</section>;
-const User = () => {
-  const contextValue = useContext(appContext);
-  return <div>User:{contextValue.appState.user.name}</div>;
+const 大儿子 = () => {
+  console.log("大儿子", Math.random());
+  return (
+    <section>
+      大儿子
+      <User />
+    </section>
+  );
+};
+const 二儿子 = () => {
+  console.log("二儿子", Math.random());
+  return (
+    <section>
+      二儿子
+      <UserModifier>内容</UserModifier>
+    </section>
+  );
+};
+const 幺儿子 = () => {
+  console.log("幺儿子", Math.random());
+  return <section>幺儿子</section>;
 };
 
 // 用reducer规范state更新(返回新的state)
@@ -55,17 +74,31 @@ const reducer = (state, { type, payload }) => {
 // 让组件和全局状态链接起来
 const connect = (Component) => {
   return (props = {}) => {
-    const { appState, setAppState } = useContext(appContext);
+    const { state, setState } = useContext(appContext);
+    // 强制刷新被connect组件
+    const [, update] = useState({});
+    // 只订阅一次
+    useEffect(() => {
+      store.subscribe(() => update({}));
+    }, []);
+
     // dispatch规范setState流程
     const dispatch = (action) => {
-      setAppState(reducer(appState, action));
+      console.log("dispatch");
+      console.log(setState);
+      console.log(reducer(state, action));
+      setState(reducer(state, action));
     };
-    return <Component {...props} dispatch={dispatch} state={appState} />;
+    return <Component {...props} dispatch={dispatch} state={state} />;
   };
 };
 
+const User = connect(() => {
+  const { state, setState } = useContext(appContext);
+  return <div>User:{state.user.name}</div>;
+});
+
 const _UserModifier = ({ dispatch, state, children }) => {
-  const { appState, setAppState } = useContext(appContext);
   const onChange = (e) => {
     dispatch({
       type: "updateUser",
@@ -75,7 +108,7 @@ const _UserModifier = ({ dispatch, state, children }) => {
   return (
     <div>
       {children}
-      <input value={appState.user.name} onChange={onChange} />
+      <input value={state.user.name} onChange={onChange} />
     </div>
   );
 };
